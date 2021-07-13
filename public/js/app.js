bece9a929da98547d6880a7f08e9c921f4d73ddd
Fazal Basit <fazal0102@gmail.com>
2021-07-13 21:16:06 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+
+console.log('Client Side JS file Loaded')
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//     response.json().then((data) => {
+//         if(data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = 'From JS'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+   
+    messageOne.text = 'Loading....'
+    messageTwo.text = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            // console.log(data.location)
+            // console.log(data.forecast)
+        }
+    })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude,longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=51c016904d0bf847b090f8813c020409&query=' + latitude + ',' + longitude + '&units=f'
+    request({url, json: true}, (error, {body}={}) => {
+        if(error){
+            callback('Unable to connect to weather services!', undefined)
+        } else if(body.error){
+            callback('unable to find location', undefined)
+        }else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out fahrenheit. It feels like ' + body.current.feelslike + ' degrees fahrenheit out. Current wind speed is ' + body.current.wind_speed)
+            
+        }
+    })
+}
+
+module.exports = forecast
+
